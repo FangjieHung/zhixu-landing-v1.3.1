b13a3b5e559467aeb2a9b2e9a65e1090ee163dd9
FangjieHung <j87345@gmail.com>
2026-04-15 11:22:22 +0800

    Fix: 刪除 animate on scroll

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,18 +3,4 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .then(async () => {
-    if (typeof window !== 'undefined') {
-      try {
-        // Dynamically import AOS on client to avoid static CommonJS import
-        const mod = await import('aos');
-        const AOS = (mod && (mod.default || mod)) as any;
-        AOS?.init?.({ once: false, offset: 120, duration: 3000 });
-      } catch (e) {
-        // Fail gracefully if AOS cannot be loaded
-        // eslint-disable-next-line no-console
-        console.warn('AOS failed to load', e);
-      }
-    }
-  })
   .catch((err) => console.error(err));
